43710c271d2762171101948761e01ebdd909dcc5
rr69sport <richardramsua@gmail.com>
2022-08-18 01:43:48 -0300

    Add url validator and status codes messages

--- index.js
+++ index.js
@@ -8,37 +8,45 @@ dotenv.config();
 const app = express();
 app.use(cors());
 
-app.get('/', (req, res) => {
-  res.send('This is a test message...')
-})
+const pattern = /^http(s)?:\/\/?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/gm;
 
 app.get('/', (req, res) => {
+
+  if (!req.query.url) res.status(200).json('Default message');
+
   const { url } = req.query;
-  axios(url)
-    .then(response => {
-      const html = response.data;
-      const elements = cheerio.load(html);
 
-      const getTypeTag = (type) => {
-        return elements(`meta[name="${type}"]`).attr('content') || elements(`meta[property="${type}"]`).attr('content')
-      }
+  if (pattern.test(url)) {
+    axios(url)
+      .then(response => {
+        const html = response.data;
+        const elements = cheerio.load(html);
 
-      const getDataFrom = (nameTag) => {
-        return getTypeTag(nameTag) || getTypeTag(`og:${nameTag}`) || getTypeTag(`twitter:${nameTag}`);
-      }
+        const getTypeTag = (type) => {
+          return elements(`meta[name="${type}"]`).attr('content') || elements(`meta[property="${type}"]`).attr('content')
+        }
 
-      const getMetadata = () => ({
-        title: elements('title').text() || getDataFrom('title'),
-        description: getDataFrom('description'),
-        image: getDataFrom('image'),
-        url: getDataFrom('url')
-      });
+        const getDataFrom = (nameTag) => {
+          return getTypeTag(nameTag) || getTypeTag(`og:${nameTag}`) || getTypeTag(`twitter:${nameTag}`);
+        }
 
-      const scrapedData = getMetadata();
+        const getMetadata = () => ({
+          title: elements('title').text() || getDataFrom('title'),
+          description: getDataFrom('description'),
+          image: getDataFrom('image'),
+          url: getDataFrom('url')
+        });
 
-      res.json(scrapedData);
+        const scrapedData = getMetadata();
 
-    }).catch(error => console.log(error));
+        res.status(200).json(scrapedData);
+
+      }).catch(error => {
+        return res.status(404).jsonp({ error: error })
+      })    
+  } else {
+    res.status(404).jsonp({ error: 'URL not valid' })
+  }
 })
 
 app.listen(process.env.PORT, () => console.log(`Server running on port: ${process.env.PORT}`));
